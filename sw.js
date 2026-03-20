bce631517561f5527fd38a6bbe52795c850cfecd
AduDarkVL <136780027+AduDarkVL@users.noreply.github.com>
2026-03-20 21:07:35 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'quoc-ky-cache-v2';
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(event.request).then((networkResponse) => {
+                if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic' && networkResponse.type !== 'cors') {
+                    return networkResponse;
+                }
+                const responseToCache = networkResponse.clone();
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, responseToCache);
+                });
+                return networkResponse;
+            }).catch(() => {
+                console.log("Mất mạng và chưa lưu ảnh này: ", event.request.url);
+            });
+        })
+    );
+});
